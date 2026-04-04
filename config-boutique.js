498940e39ef92fbe2a20b9b35c7f16305d601ec9
Muzafari Sayed Basharat Reza <150054270+SayedBM@users.noreply.github.com>
2026-04-04 22:57:04 +0200

    Update boutique configuration and status logicTest

--- config-boutique.js
+++ config-boutique.js
@@ -1,11 +1,11 @@
 // === CONFIGURATION DE LA BOUTIQUE ===
 const boutiqueConfig = {
   // 1. VACANCES ET FERMETURES
-  estEnVacances: true,
+  estEnVacances: false,
   messageVacances:
     "We are closed today!,
 
-  fermetureExceptionnelle: false,
+  fermetureExceptionnelle: true,
   messageExceptionnel: "this morning we are closed.",
 
   // 2. HORAIRES (Format 24h : [Ouverture, Fermeture])
